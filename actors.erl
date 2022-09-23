d8a6063adbe2c85b5cb13114356ad798aa4c44fc
Reecha Khanal <reecha.khanal@gmail.com>
2022-09-22 23:22:38 -0400

    Added String Comparision Functionality, To check leading # of leading zeros

--- actors.erl
+++ actors.erl
@@ -1,30 +1,59 @@
 %% @author
 -module(actors).
 
-% -import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/1]).
+-import(string, [substr/3, equal/2]).
 -export([main/0]).
 
 main() ->
-    get_data(),
+    X = get_data(),
     server(),
-    worker(),
-    randomStringGenerator_SHA256().
+    worker(X).
 
 get_data() ->
-    {ok, [X]} = io:fread("Enter the number of zeros: ", "~d\n"),
-    io:format("The Number User Entered was: ~w\n", [X]).
+    {ok, [X]} = io:fread("\nEnter the number of zeros: ", "~d\n"),
+    X.
 
 server() ->
     'Entered Server Code'.
 
-worker() ->
-    'Entered Server Code'.
+worker(NumberOfZeros) ->
+
+    HashString = randomStringGenerator_SHA256(),
+    % Check if we have the desired number of zeros
+    Check = checkStringMatch(NumberOfZeros, HashString),
+    if 
+        Check == true ->
+            io:fwrite("\n\nYAAASSSSSS :) :)\n\n");
+        true -> 
+            io:fwrite("\n\nOOPSIE, No luck :( \n\n") 
+    end.
 
 randomStringGenerator_SHA256() ->
-    Random_Generated_String = base64:encode(crypto:strong_rand_bytes(6)),
 
     % generates random string
+    Random_Generated_String = base64:encode(crypto:strong_rand_bytes(6)),
     io:format("The random generated string is: ~s\n", [Random_Generated_String]),
 
-    %passing the above generated string to a  SHA256
-    io_lib:format("Hash Generated: ~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,Random_Generated_String))]).
+    %passing the above generated string to a SHA256 and printing the generated hash value
+    HashString = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,Random_Generated_String))]),
+    io:fwrite(HashString),  
+
+    HashString. % return Hash String Generated    
+
+checkStringMatch(NumberOfZeros, HashString) ->
+
+    % Check and first 'Number_Of_Zeros' digits of the generated hash function
+    PotentialZeros = substr(HashString, 1, NumberOfZeros),
+    %io:fwrite(PotentialZeros),
+
+    %% create a target string we desire,
+    %% Here TargetString would be "00" if the Number of Zeros was 2.
+    TargetString =  lists:flatten(lists:duplicate(NumberOfZeros, "0")),
+    %io:fwrite(TargetString),
+
+    %% Check if the hash contains desired leading zeros, string comparision
+    Status = equal(PotentialZeros, TargetString), 
+    %io:fwrite("~p~n\n",[Status]),
+    
+    % return result of the string match check
+    Status.

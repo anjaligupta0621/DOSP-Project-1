0eae7d321e9e5725ea35f7aa7225006a16d89137
Reecha Khanal <reecha.khanal@gmail.com>
2022-09-24 16:40:33 -0400

    Added TCP code and client server connection.

--- client.erl
+++ client.erl
@@ -1,28 +1,35 @@
--module(clients).
+-module(client).
 
 -import(string, [substr/3, equal/2]).
--export([main/2]).
+-export([start/0, startMining/3]).
 
-main(X, Id) ->
-    %timer:sleep(1000 * rand:uniform(5)),
+start() ->
+    IPAddress = "localhost",
+    PortNumber = 1204,
+    % you can pass localhost for now.
+    {ok, Sock} = gen_tcp:connect(IPAddress, PortNumber, [binary, {packet, 0}]),
+    ok = gen_tcp:send(Sock, "I want work"),
+    startWorkers(1, 4, Sock).
+    %ok = gen_tcp:close(Sock).
+
+startWorkers(X, N, Sock) ->
+    Workers = [  % { {Pid, Ref}, Id }
+        { spawn(client, startMining, [X, Id, Sock]), Id }
+        || Id <- lists:seq(1, N)
+    ],
+    Workers.
+
+startMining(X, Id, Sock) ->
     io:format("Worker~w: I'm still alive~n", [Id]),
     % start time
-
     T1 = erlang:time(),
-    %T2 = erlang:timestamp(),
-    %io:fwrite("\n"),
     io:fwrite("Time of worker ~w -> ~w \n",[Id, T1]),
-    worker(X),
+    worker(X, Sock),
     T2 = erlang:time(),
     io:fwrite("Time 2 of worker ~w -> ~w \n", [Id, T2]),
-
-    %DiffT = now_diff(T2,T1),
-    %io:fwrite(DiffT),
-    % end time
-    % print time taken
     X.
 
-worker(NumberOfZeros) ->
+worker(NumberOfZeros, Sock) ->
     
     Random_Generated_String = randomStringGenerator(),
     HashString = run_SHA256(Random_Generated_String),
@@ -32,44 +39,34 @@ worker(NumberOfZeros) ->
     % Recursive Loop until we find the right number of leading zeros
     if 
         Check == true ->
-            %io:fwrite("\n\nYAAASSSSSS :) :)\n\n"),
-            io:format("OUTPUT: anjaligupta:~s ~s\n", [Random_Generated_String, HashString]);
+            Coin_Found = io_lib:format("\nanjaligupta:~s ~s\n", [Random_Generated_String, HashString]),
+            
+            %STR3 = concat(concat(Random_Generated_String," "), HashString),
+            %STR3 = Random_Generated_String ++ " " + HashString,
+            gen_tcp:send(Sock, Coin_Found);
         true -> 
-            %io:fwrite("\n\nOOPSIE, No luck :( \n\n"),
-            worker(NumberOfZeros)
+            worker(NumberOfZeros, Sock)
     end.
 
 randomStringGenerator() ->
     % generates and prints the random string
     Random_Generated_String = base64:encode(crypto:strong_rand_bytes(6)),
-    %io:format("The random generated string is: ~s\n", [Random_Generated_String]),
-
     Random_Generated_String. % return the randomly generated string
 
 run_SHA256(Random_Generated_String) ->
 
     %passing the above generated string to a SHA256 and printing the generated hash value
     HashString = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,Random_Generated_String))]),
-    %io:fwrite(HashString),  
-
     HashString. % return Hash String Generated    
 
 checkStringMatch(NumberOfZeros, HashString) ->
 
     % Check and first 'Number_Of_Zeros' digits of the generated hash function
     PotentialZeros = substr(HashString, 1, NumberOfZeros),
-    % io:fwrite("\n"),
-    % io:fwrite(PotentialZeros),
-    % io:fwrite("\n"),
-
     %% create a target string we desire,
-    %% Here TargetString would be "00" if the Number of Zeros was 2.
     TargetString =  lists:flatten(lists:duplicate(NumberOfZeros, "0")),
-    % io:fwrite(TargetString),
 
-    %% Check if the hash contains desired leading zeros, string comparison
+    %% Check if the hash contains desired leading zeros, string comparision
     Status = equal(PotentialZeros, TargetString), 
-    %io:fwrite("~p~n\n",[Status]),
-    
     % return result of the string match check
     Status.
--- server.erl
+++ server.erl
@@ -1,30 +1,103 @@
 %% @author
 -module(server).
-
 -import(string, [substr/3, equal/2]).
--export([main/0]).
+-export([start/0, server/0, startMining/2]).
 
-main() ->
+start() ->
+    N= 4,
     X = get_data(),
-    server(X).
+    createWorkers(N, X),
+    server().
 
 get_data() ->
     {ok, [X]} = io:fread("\nEnter the number of zeros: ", "~d\n"),
     X.
 
-server(X) ->
-    io:fwrite("\nEntered Server Code\n"),
-    createWorkers(6, X).
-    %PID = spawn(clients, main, [X]),
-    %io:fwrite(PID).
+server() ->
+    % server starts by listening on a specified port number
+    {ok, ListenSocket} = gen_tcp:listen(1204, [binary, {packet, 0}, {active, false}]),
+    {ok, Sock} = gen_tcp:accept(ListenSocket), 
+    loop(Sock),
+    ok = gen_tcp:close(Sock),
+    ok = gen_tcp:close(Sock).
+
+loop(Sock) ->
+    % server accepts a client connection whenever it gets one
+    % receive data sent by client
+    inet:setopts(Sock,[{active,once}]),
+    receive
+        {tcp,S,Data} ->
+            %Answer = process(Data), % Not implemented in this example
+
+            Work = "I want work",
+            Status = equal(Data, Work),
+
+            if
+                Status == true ->
+                    io:fwrite("A worker is available for some work\n");
+                true ->
+                    io:format("~s", [Data])
+                    %io:fwrite(Data)
+            end,
+
+
+            %gen_tcp:send(S,Answer),
+            loop(S);
+        {tcp_closed,S} ->
+            io:format("Socket ~w closed [~w]~n",[S,self()]),
+            ok
+    end.
 
 createWorkers(N, X) ->
-
     Workers = [  % { {Pid, Ref}, Id }
-        { spawn(clients, main, [X, Id]), Id }
+        {spawn(server, startMining, [X, Id]), Id }
         || Id <- lists:seq(1, N)
     ],
     Workers.
-    %monitor_workers(Workers).
 
+startMining(X, Id) ->
+    io:format("Worker~w: I'm still alive~n", [Id]),
+    % start time
+    T1 = erlang:time(),
+    io:fwrite("Time of worker ~w -> ~w \n",[Id, T1]),
+    worker(X),
+    T2 = erlang:time(),
+    io:fwrite("Time 2 of worker ~w -> ~w \n", [Id, T2]),
+    X.
 
+worker(NumberOfZeros) ->
+    Random_Generated_String = randomStringGenerator(),
+    HashString = run_SHA256(Random_Generated_String),
+    % Check if we have the desired number of zeros
+    Check = checkStringMatch(NumberOfZeros, HashString),
+
+    % Recursive Loop until we find the right number of leading zeros
+    if 
+        Check == true ->
+            io:format("\nanjaligupta:~s ~s\n", [Random_Generated_String, HashString]);
+        true -> 
+            worker(NumberOfZeros)
+    end.
+
+randomStringGenerator() ->
+    % generates and prints the random string
+    Random_Generated_String = base64:encode(crypto:strong_rand_bytes(6)),
+    Random_Generated_String. % return the randomly generated string
+
+run_SHA256(Random_Generated_String) ->
+
+    %passing the above generated string to a SHA256 and printing the generated hash value
+    HashString = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256,Random_Generated_String))]),
+    HashString. % return Hash String Generated    
+
+checkStringMatch(NumberOfZeros, HashString) ->
+
+    % Check and first 'Number_Of_Zeros' digits of the generated hash function
+    PotentialZeros = substr(HashString, 1, NumberOfZeros),
+    %% create a target string we desire,
+    TargetString =  lists:flatten(lists:duplicate(NumberOfZeros, "0")),
+
+    %% Check if the hash contains desired leading zeros, string comparision
+    Status = equal(PotentialZeros, TargetString), 
+    % return result of the string match check
+    Status.
